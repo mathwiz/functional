47d68c3f01082d943de83c1775ac495656b5ca37
mathwiz <mathwiz@gmail.com>
2016-06-28 21:16:09 -0500

    Final recfun

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,7 +14,7 @@ object Main {
     * Exercise 1
     */
   def pascal(c: Int, r: Int): Int = {
-    if (r==c || c == 0 || r == 0) 1
+    if (r == c || c == 0 || r == 0) 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
@@ -22,10 +22,23 @@ object Main {
   /**
     * Exercise 2
     */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def count(acc: Int, xs: List[Char]): Int = {
+      if (xs.isEmpty || acc < 0) acc
+      else if (xs.head.equals(')')) count(acc - 1, xs.tail)
+      else if (xs.head.equals('(')) count(acc + 1, xs.tail)
+      else count(acc, xs.tail)
+    }
+
+    count(0, chars) == 0
+  }
 
   /**
     * Exercise 3
     */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money < 0 || coins.isEmpty) 0
+    else if (money == 0) 1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
 }
--- recfun/src/test/scala/recfun/Scratch.sc
+++ recfun/src/test/scala/recfun/Scratch.sc
@@ -5,3 +5,28 @@ recfun.Main.pascal(2,1);
 recfun.Main.pascal(2,2);
 recfun.Main.pascal(2,3);
 
+val b = ":-)".toList;
+val a= ")".toList
+recfun.Main.balance(b);
+
+def count(acc: Int, xs: List[Char]) : Int = {
+  if (xs.isEmpty || acc < 0) acc
+  else if (xs.head.equals(')')) count(acc - 1, xs.tail)
+  else if (xs.head.equals('(')) count(acc + 1, xs.tail)
+  else count(acc, xs.tail)
+}
+
+count(0, b);
+count(0, a);
+a.head;
+a.head.equals(')');
+
+def countChange(money: Int, coins: List[Int]): Int = {
+    if (money < 0 || coins.isEmpty) 0
+    else if (money == 0) 1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+}
+
+
+countChange(4, List(1,2));
+
