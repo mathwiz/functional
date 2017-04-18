04fd83dd8aba6a6719c23ce7c4810595b69e219f
mathwiz <mathwiz@gmail.com>
2017-04-18 15:32:29 -0400

    GUI example

---
+++ ScalaTDD/src/net/learningcity/gui/UI.scala
@@ -0,0 +1,20 @@
+package net.learningcity.gui
+
+/**
+  * Created by Yohan on 4/18/2017.
+  */
+import scala.swing._
+
+class UI extends MainFrame {
+  title = "GUI Program #1"
+  preferredSize = new Dimension(320, 240)
+  contents = new Label("Here is the contents!")
+}
+
+object GuiProgramOne {
+  def main(args: Array[String]) {
+    val ui = new UI
+    ui.visible = true
+    println("End of main function")
+  }
+}
